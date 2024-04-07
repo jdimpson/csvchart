dfe9dcd9a35e77016b73722b650a8aa7434f2329
Jeremy D. Impson <github@ur.selfip.net>
2024-04-07 17:40:08 -0400

    initial push to github

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # csvchart
-python script that convers CSV files into SVG images
+python script that converts CSV files into SVG images
+
+
+Requires python's builtin `csv` module and the `pygal` module.
+
+Turns one or more CSV files into SVG image charts. Can concatenate multiple CSV files into one lgical database, from which it can chart mutiple values as different charted lines. Exposes a fair amount of the `pygal` functionality. although documentation is terrible.
+
+## Examples
+
+[Example 1](examples/ex1) Chart temperature and humidity from one logical data base, contained in three CSV files.
+
+The start of `log1.csv` looks like this:
+```
+date,time,tempC,tempF,pressurePA,presureMB,light,R,G,B,heading,magnetometer0,magnetometer1,magnetometer2,accelX,accelY,accelZ,anal0,anal1,anal2,anal3,leds
+2018-03-11,18:18,26.91,80.44,98553.97,985.54,61,104,112,125,336.97,2089,-1203,-5017,-0.01,0.05,1.0,0.561,0.543,0.549,0.585,OFF
+2018-03-11,18:35,27.12,80.81,98580.4,985.8,245,106,83,94,336.57,2098,-1197,-4989,-0.01,0.05,1.0,0.516,0.57,0.582,0.555,ON
+2018-03-11,19:05,26.77,80.18,98598.58,985.99,396,165,133,137,335.08,2100,-1212,-4998,-0.0,0.05,0.99,0.549,0.558,0.537,0.612,ON
+```
+
+`log2.csv` and `log3.csv` have the same fields, and all files have disjoint data (no overlaps--they represent three days worth of readings).
+
+We can create a chart of the temperature using this command:
+
+```
+csvchart.py \
+"csv:file=log1.csv,log2.csv,log3.csv;x-field=date,time;y-fields=tempC;labels=Celcius" \
+"chart:file=example1.svg;title=Example 1 temperature readings;interpolation=cubic;"
+```
+
+Note the structure of this command, The `csvchart.py` command takes two (or more) arguments. The first is a "csv:..." argument, and it specifies the data source or sources. The rest of this argument are key/value pairs seperated by semicolons. The values may be lists, which are separted by commas. There's plenty to say here, but the most salient commaents are that the `file=` key accepts a list of CSV files that needs to have disjoint data ranges. The `x-field=` will be interepretted as the single X-axis variable. It can be multiple values, which will be concatenated together. This allows you to use CSV records where the `date` and `time` are seperate fields. Whereas the `y-fields` can also be alist, but each value will be charted separately (e.g. temperature vs humidity). If you have multiple y-fields, you can have multiple labels, and each label will be used for each Y value, in the same order as they are listed.
+
+The second argument is the "chart:..." argument. It specifies how the resulting SVG chart looks. In this argument, most of the values are singletons, not list. For example, the `file=` key/value will specify the single SVG output file. Note that the [chosen interpolation](https://www.pygal.org/en/3.0.0/documentation/configuration/interpolations.html) can be very misleading. Right now, `csvchart` lets you choose any of the interpolation systels, but does not yet expose further attributes like `interpolation_parameters` or `interpolation_precision`.
+
+The result of the above command on the example 1 data looks like [<img src="examples/ex1/example1.svg">](examples/ex1/example1.svg").
---
+++ csvchart.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+
+import csv
+import pygal
+
+class csvchart(object):
+	def usage(self,argv0):
+		return '''Usage: {} csv:METADATA1 csv:METADATA2 ... chart:file=chart.svg
+where:
+	METADATAx is a semicolon-separated list that identifies a CSV file, what fields to add to the visualization chart, and how to label them, e.g.
+		csv:file=/path/to/data/file1.csv;x-field=date,time;y-fields=tempC csv:file=/path/to/data/file2.csv;x-field=date,time;y-fields=tempC,humidity;sides=left,right
+'''.format(argv0)
+
+	def parseargv(self, argv):
+		self.sources = []
+		chart = None
+		labels = []
+		for x in argv:
+			if   x.startswith('csv:'):
+				self._parsecsvmeta(x[4:])
+			elif x.startswith('chart:'):
+				self._parsechartmeta(x[6:])
+			else:
+				raise RuntimeError("unrecognized argument {}".format(x))
+		if not hasattr(self,'chart'):
+			self.chart=None
+
+	def _parsemeta(self,m):
+		if not m.endswith(';'):
+			m = m + ';'
+		tmp = {}
+		for x in m.split(';'):
+			if x == '': continue
+			i = x.find('=')
+			if i == -1:
+				raise RuntimeError("error parsing metadata {}".format(x))
+			l=x[:i]
+			f=x[i+1:]
+			if ',' in f:
+				f = f.split(',')
+			else:
+				f = [f]
+			if l in tmp:
+				raise RuntimeError("second definition of {} in metadata".format(l))
+			tmp[l]=f
+		return tmp
+
+	def _parsechartmeta(self,m):
+		if hasattr(self,'chart'):
+			raise RuntimeError("second definition of chart: {}".format(m))
+		self.chart = self._parsemeta(m)
+		self.chart['file']  = self.chart['file'][0]
+		self.chart['title'] = self.chart['title'][0]
+		if 'recwidth' in self.chart:
+			self.chart['recwidth'] = int(self.chart['recwidth'][0])
+		else:
+			self.chart['recwidth'] = 10
+		t = 'Line'
+		if 'type' in self.chart:
+			t = self.chart['type'][0]
+		if   t == 'HorizontalBar':  self.chart['type'] = pygal.HorizontalBar
+		elif t == 'StackedBar':  self.chart['type'] = pygal.StackedBar
+		elif t == 'Bar':  self.chart['type'] = pygal.Bar
+		elif t == 'HorizontalLine': self.chart['type'] = pygal.HorizontalLine
+		elif t == 'StackedLine': self.chart['type'] = pygal.StackedLine
+		elif t == 'Line': self.chart['type'] = pygal.Line
+		else: self.chart['type'] = pygal.Line
+		
+		if 'interpolation' in self.chart:
+			self.chart['interpolation'] = self.chart['interpolation'][0]
+		else:
+			self.chart['interpolation'] = None
+		if 'secondary_range' in self.chart:
+			self.chart['secondary_range'] = [float(v) for v in self.chart['secondary_range'] ]
+		else:
+			self.chart['secondary_range'] = None
+		if 'value' in self.chart:
+			self.chart['value'] = self.chart['value'][0]
+		else:
+			self.chart['value'] = 'last'
+		if self.chart['value'] == 'average':
+			self.chart['value'] = 'mean'
+			# mean, total, min, max, count, first, last, confidence, maxconfidence
+
+
+	def _parsecsvmeta(self,m):
+		tmp = self._parsemeta(m)
+		#tmp['x-field'] = tmp['x-field'][0]
+		#tmp['file'] = tmp['file'][0]
+		if not 'labels' in tmp:
+			tmp['labels'] = tmp['y-fields']
+		if not 'sides' in tmp:
+			tmp['sides'] = ['left'] * len(tmp['labels'])
+		
+		self.sources.append(tmp)
+
+	def summarize(self, preval, newval):
+		preval['last'] = newval
+		preval['count'] += 1
+		if newval > preval['max']: preval['max'] = newval
+		if newval < preval['min']: preval['min'] = newval
+		preval['total'] += newval
+		preval['mean'] = preval['total'] / preval['count']
+		return preval
+
+	def makedata(self):
+		self.data = {}
+		self.seenlabels = {} # global (this feels like a hack)
+		for s in self.sources: # local
+			seenlabels = {}
+			fs=s['file']
+			for f in fs:
+				reader = csv.DictReader(open(f,'r'))
+				try:
+					for r in reader:
+						#x = r[s['x-field']]
+						x = '-'.join([r[k] for k in s['x-field']])
+						if x not in self.data:
+							self.data[x] = {}
+						for f,l,c in zip(s['y-fields'],s['labels'],s['sides']):
+							if not l in seenlabels:
+								seenlabels[l]=c
+							y = float(r[f])
+							if l in self.data[x]:
+								#print(f"already have {l} in {x}: {y}")
+								self.data[x][l] = self.summarize(self.data[x][l], y)
+							else:
+								#print(f"adding {l} in {x}: {y}")
+								self.data[x][l] = {'mean': y, 'total': y, 'min': y, 'max': y, 'count': 1, 'first': y, 'last': y}
+				except Exception as e:
+					print(s,e)
+					raise e
+			for l in seenlabels: # locally seen
+				if l in self.seenlabels: # globally seen
+					raise RuntimeError("Reuse of label {} from another dataset!".format(l))
+				else:
+					self.seenlabels[l] = seenlabels[l]
+				
+		# fill in sparse datasets
+		for s in self.data:
+			for l in self.seenlabels:
+				if l not in self.data[s]:
+					#self.data[s][l] = None
+					self.data[s][l] = {'mean': None, 'total': None, 'min': None, 'max': None, 'count': 0, 'first': None, 'last': None}
+
+					#d = r['date']
+					#h = r['time'][:5]
+					#s = d+"-"+h
+
+
+
+if __name__ == '__main__':
+	import sys
+	from pygal.style import Style
+
+	viz = csvchart()
+	if len(sys.argv) <= 1:
+		print(viz.usage(sys.argv[0]))
+		exit(1)
+	viz.parseargv(sys.argv[1:])
+	#print("sources")
+	#for s in viz.sources: print("\t",s)
+	if viz.chart is None:
+		print("no chart provided")
+		print(viz.usage(sys.argv[0]))
+		exit(2)
+	#print("chart")
+	#print("\t",viz.chart)
+	viz.makedata()
+	#print("chart")
+	#print("\t",viz.data)
+
+	custom_style = Style(
+		colors=('#0343df', '#e50000', '#929591', '#ffff14'),
+		font_family='Roboto,Helvetica,Arial,sans-serif',
+		background='transparent',
+		label_font_size=14,
+	)
+
+	x_labels = list(viz.data.keys())
+	width = len(x_labels) * viz.chart['recwidth']
+	if width < 1200:
+		width = 1200
+	#c = pygal.Line(
+	c = viz.chart['type'](
+		interpolate=viz.chart['interpolation'],
+		title=viz.chart['title'],
+		style=custom_style,
+		#y_title=temp_unit,
+		width=width,
+		secondary_range=viz.chart['secondary_range'],
+		x_label_rotation=300,
+	)
+	
+	c.x_labels = sorted(x_labels)
+	value = viz.chart['value']
+
+	def guido(x,l):
+		if value == "confidence":
+			tmpvalue='mean'
+			ci={'low':viz.data[x][l]['min'], 'high':viz.data[x][l]['max'] }
+		elif value == "maxconfidence":
+			tmpvalue='max'
+			ci={'low':viz.data[x][l]['min'], 'high':viz.data[x][l]['max'] }
+		elif value == "minconfidence":
+			tmpvalue='min'
+			ci={'low':viz.data[x][l]['min'], 'high':viz.data[x][l]['max'] }
+		else:
+			tmpvalue=value
+			ci=None
+		if viz.data[x][l][tmpvalue] is None: 
+			return None # NOTE: need this to support incomplate lines
+		o = {'value': round(viz.data[x][l][tmpvalue],2) }
+		if ci is not None:
+			o['ci'] = ci
+		return o
+	v = guido
+
+	for l in viz.seenlabels:
+		secondary = viz.seenlabels[l]=='right'
+		c.add(l, [v(x,l) for x in c.x_labels], allow_interruptions=True, secondary=secondary)
+	
+	c.render_to_file(viz.chart['file'])
+	exit(0)
